c5b8be6b0a669f7cccbc50b074ddb3c9d065913b
narms <75282394+n-arms@users.noreply.github.com>
2021-02-26 17:54:51 -0500

    change logic to allow for multiple definitions for one citation type

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import json
 
+def match_format(data, format):
+    for i in format:
+        if i not in data:
+            return False
+    return True
+
 def cite(data, style):
     with open("cite-logic.json") as config_file:
         format = json.load(config_file)[style]
-    template = format["text"]
-    for regex in format["regexes"]:
-        if regex in data:
-            template = template.replace(regex, data[regex])
-        else:
-            template = template.replace(regex, "")
-    return template
 
-print(cite({"Name": "Smith J.", "Year": "0000", "Month": "0", "Date": "0", "Title": "A treatise on the evolution of yaks", "Site": "yaks global", "URL": "yaks-global.com"}, "APA"))
+    for i in format:
+        if match_format(data, i["requirements"]):
+            output = i["text"]
+            for key in data:
+                output = output.replace(key, data[key])
+            return output
+
+print(cite({"Lastname": "Smith",
+"Initials": "J. Q.",
+"Year": "2000",
+"Month": "January",
+"Date": "10",
+"Title": "A treatise on the evolution of yaks",
+"Site": "yaks global", "URL": "yaks-global.com"},
+"APA"))
