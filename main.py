ffa7f24867b58eb5af16c8d63049e5acd0b88854
narms <75282394+n-arms@users.noreply.github.com>
2021-02-26 20:25:09 -0500

    Clean up GUI

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ import json
 from tkinter import *
 
 def match_format(data, format):
-    print("matching")
     for i in format:
         if i not in data:
-            print("failed on", i)
             return False
     return True
 
@@ -38,20 +36,31 @@ class GUIFrame(Frame):
         "Group Name": "Group",
         "Site Name": "Site",
         "URL": "URL"}
-        self.entries = [(Label(self, text=i), Entry(self, text='')) for i in self.pretty_name]
+        self.entries = [(Label(self, text=i, font="arial 15"), Entry(self, text='', font="arial 15")) for i in self.pretty_name]
         for i in range(len(self.entries)):
             self.entries[i][0].grid(row=i, column=0)
             self.entries[i][1].grid(row=i, column=1)
-        self.citation_box = Label(self, text="Enter some data for a citation!")
-        self.citation_box.grid()
-        self.cite_button = Button(self, text="cite", command=self.generate_citation)
-        self.cite_button.grid()
+        self.citation_box = Text(self, wrap='word', height=3)
+        self.citation_box.insert('end', "Enter some data for a citation!")
+        self.citation_box['state'] = 'disabled'
+        self.citation_box.grid(row=len(self.entries), column=0, columnspan=2)
+        self.citation_box.tag_config("normal", font="arial 15")
+        self.citation_box.tag_add("normal", "1.0", "end")
+
+
+        self.cite_button = Button(self, text="cite", command=self.generate_citation, font="arial 15")
+        self.cite_button.grid(row=len(self.entries)+1, column=0)
 
     def generate_citation(self):
         data = {}
         for i in self.entries:
-            data[self.pretty_name[i[0]["text"]]] = i[1].get()
-        self.citation_box['text'] = cite(data, "APA") or (self.citation_box['text']+"'")
+            if i[1].get() != '':
+                data[self.pretty_name[i[0]["text"]]] = i[1].get()
+        self.citation_box['state'] = "normal"
+        self.citation_box.delete('1.0', 'end')
+        self.citation_box.insert("end", cite(data, "APA") or "please enter more data")
+        self.citation_box.tag_add("normal", "1.0", "end")
+        self.citation_box['state'] = 'disabled'
 
 
 root = Tk()
