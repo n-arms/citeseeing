b4894cb13f8d3f2af62267b86673f0ad073876b5
narms <75282394+n-arms@users.noreply.github.com>
2021-02-26 19:14:20 -0500

    Add class for GUI, add text entries

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+#!/usr/bin/env python3
 import json
+from tkinter import *
 
 def match_format(data, format):
     for i in format:
@@ -17,11 +19,17 @@ def cite(data, style):
                 output = output.replace(key, data[key])
             return output
 
-print(cite({"Lastname": "Smith",
-"Initials": "J. Q.",
-"Year": "2000",
-"Month": "January",
-"Date": "10",
-"Title": "A treatise on the evolution of yaks",
-"Site": "yaks global", "URL": "yaks-global.com"},
-"APA"))
+class GUIFrame(Frame):
+    def __init__(self, master):
+        Frame.__init__(self, master)
+        self.grid()
+        self.pretty_name = {"Lastname": "Last Name", "rMonth": "Retrieved (Month)", "rYear": "Retrieved (Year)", "rDate": "Retrieved (Date)", "Initials": "Initials", "Month": "Published (Month)", "Year": "Published (Year)", "Date": "Published (Date)", "Title": "Title", "Group": "Group Name", "URL": "URL"}
+        print(self.pretty_name)
+        self.entries = [(Label(self, text=self.pretty_name[i]), Entry(self, text='')) for i in self.pretty_name]
+        for i in range(len(self.entries)):
+            self.entries[i][0].grid(row=i, column=0)
+            self.entries[i][1].grid(row=i, column=1)
+
+root = Tk()
+f = GUIFrame(root)
+root.mainloop()
