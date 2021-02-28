caff60d532c809f6da462a58f992efd796369568
narms <75282394+n-arms@users.noreply.github.com>
2021-02-27 20:38:18 -0500

    Pretty up GUI

--- main.py
+++ main.py
@@ -22,6 +22,8 @@ def cite(data, style):
 class GUIFrame(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
+        self.title = Label(self, text="CITESEEING", font="arial 36")
+        self.title.grid(row=0, column=0, columnspan=2)
         self.grid()
         self.pretty_name = {
         "Last Name": "Lastname",
@@ -38,12 +40,12 @@ class GUIFrame(Frame):
         "URL": "URL"}
         self.entries = [(Label(self, text=i, font="arial 15"), Entry(self, text='', font="arial 15")) for i in self.pretty_name]
         for i in range(len(self.entries)):
-            self.entries[i][0].grid(row=i, column=0)
-            self.entries[i][1].grid(row=i, column=1)
-        self.citation_box = Text(self, wrap='word', height=3)
+            self.entries[i][0].grid(row=i+1, column=0)
+            self.entries[i][1].grid(row=i+1, column=1)
+        self.citation_box = Text(self, wrap='word', height=3, width=50)
         self.citation_box.insert('end', "Enter some data for a citation!")
         self.citation_box['state'] = 'disabled'
-        self.citation_box.grid(row=len(self.entries), column=0, columnspan=2)
+        self.citation_box.grid(row=len(self.entries)+2, column=0, columnspan=2)
         self.citation_box.tag_config("normal", font="arial 15")
         self.citation_box.tag_config("italic", font="arial 15 italic")
         self.citation_box.tag_add("normal", "1.0", "end")
@@ -58,9 +60,9 @@ class GUIFrame(Frame):
                 data[self.pretty_name[i[0]["text"]]] = i[1].get()
         self.citation_box['state'] = "normal"
         self.citation_box.delete('1.0', 'end')
-        self.citation_box.insert("end", cite(data, "APA") or "please enter more data")
+        self.citation_box.insert("end", cite(data, "APA") or "Please enter more data")
         self.citation_box.tag_add("normal", "1.0", "end")
-        self.citation_box['state'] = 'disabled'
+        self.citation_box['state'] = 'normal'
         self.parse_italics()
 
     def parse_italics(self):
@@ -87,9 +89,10 @@ class GUIFrame(Frame):
         self.citation_box.tag_add('normal', '1.0', 'end')
         for i in tags:
             self.citation_box.tag_add('italic', f"1.{i[0]}", f"1.{i[1]}")
-        self.citation_box['state'] = 'disabled'
+        self.citation_box['state'] = 'normal'
 
 
 root = Tk()
+root.resizable(False, False)
 f = GUIFrame(root)
 root.mainloop()
